9aa7b9877665eae322cfcfb6524354d26c2e4a0a
beemo-at <jan-robin@aumann.org>
2019-11-27 21:36:06 +0100

    add intro route to get back to features before project switch

--- actions/app.go
+++ actions/app.go
@@ -61,6 +61,8 @@ func App() *buffalo.App {
 
 		app.GET("/", HomeHandler)
 
+		app.GET("/intro", IntroductionHandler)
+
 		app.ServeFiles("/", assetsBox) // serve files from the public directory
 	}
 
